7a7578e9e1fe0cbc1df43ba76d0488539ebfac15
crazybki <41273107+crazybki@users.noreply.github.com>
2021-03-22 12:48:10 +0100

    css updates

--- contactform.js
+++ contactform.js
@@ -1,1 +1,34 @@
 const emailApi = "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";
+
+const firstName = document.querySelector('.firstname');
+const lastName = document.querySelector('.lastname');
+const emailAdress = document.querySelector('.emailadress');
+const thanksmessage = document.querySelector('.firstname');
+
+
+firstName.addEventListener('input', checkFirstName);
+
+function checkFirstName() {
+    const firstNameReg = /^[a-z/A-Z\-]{3,}$/;
+    let userInput = firstName.value;
+    let checkName = firstNameReg.test(userInput);
+
+    if (checkName) {
+        thanksmessage.style.border = 'green';
+
+    } else {
+        thanksmessage.innerHTML = `please enter a real name`;
+    }
+};
+
+lastName.addEventListener('input', checkLastName);
+
+function checkLastName() {
+    const lastNameReg = /^[a-z/A-Z\-]{3,}$/;
+    let lastNameInput = lastName.value;
+}
+
+function message() {
+
+}
+
