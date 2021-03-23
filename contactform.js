f6616cf95e73c749523ea6f41074a37ace8ac7b8
crazybki <41273107+crazybki@users.noreply.github.com>
2021-03-23 16:00:45 +0100

    update formvalidator

--- contactform.js
+++ contactform.js
@@ -4,70 +4,32 @@ const firstName = document.querySelector('.firstname');
 const lastName = document.querySelector('.lastname');
 const emailAdress = document.querySelector('.emailadress');
 const thanksmessage = document.querySelector('.firstname');
+const getAllElements = document.querySelectorAll('.formElements');
 
+getAllElements.forEach(formElements => {
+    formElements.addEventListener('input', validator);
+});
 
-firstName.addEventListener('input', checkFirstName);
+function validator() {
+    const txtInputReg = /^[a-z/A-Z\-]{3,}$/;
+    const emailInputReg = /\S+@\S+\.\S+/;
+    let textFromUser = this.value
+    let emailFromuser = this.value
+    let checkEmail = emailInputReg.test(emailFromuser)
+    let checkInput = txtInputReg.test(textFromUser)
 
-function checkFirstName() {
-    const firstNameReg = /^[a-z/A-Z\-]{3,}$/;
-    let userInput = firstName.value;
-    let checkName = firstNameReg.test(userInput);
-
-    if (checkName) {
-        firstName.style.borderColor = 'green';
-        firstName.style.borderWidth = '2px';
+    if (checkInput || checkEmail) {
+        this.style.borderColor = 'green';
+        this.style.borderWidth = '2px';
     } else {
-        firstName.style.borderColor = 'red';
-        firstName.style.borderWidth = '2px';
+        this.style.borderColor = 'red';
+        this.style.borderWidth = '2px';
     }
 
-    if (userInput < 2) {
-        firstName.style.borderColor = '#6C6E66';
-        firstName.style.borderWidth = '2px';
-    }
-};
-
-
-lastName.addEventListener('input', checkLastName);
-
-function checkLastName() {
-    const lastNameReg = /^[a-z/A-Z\-]{3,}$/;
-    let lastNameInput = lastName.value;
-    let lastNameCheck = lastNameReg.test(lastNameInput);
-
-    if (lastNameCheck) {
-        lastName.style.borderColor = 'green';
-        lastName.style.borderWidth = '2px';
-    } else {
-        lastName.style.borderColor = 'red';
-        lastName.style.borderWidth = '2px';
-    }
-
-    if (lastNameInput < 2) {
-        lastName.style.borderColor = '#6C6E66';
-        lastName.style.borderColor = '2px';
+    if (textFromUser < 4) {
+        this.style.borderColor = '#6C6E66';
+        this.style.borderWidth = '2px';
     }
 }
 
 
-emailAdress.addEventListener('input', checkEmail);
-
-function checkEmail() {
-    const emailReg = /\S+@\S+\.\S+/;
-    let emailInput = emailAdress.value;
-    let emailCheck = emailReg.test(emailInput);
-    console.log(emailCheck)
-
-    if (emailCheck) {
-        emailAdress.style.borderColor = 'green';
-        emailAdress.style.borderWidth = '2px';
-    } else {
-        emailAdress.style.borderColor = 'red';
-        emailAdress.style.borderWidth = '2px'
-    }
-
-    if (emailInput < 5) {
-        emailAdress.style.borderColor = '#6C6E66';
-        emailAdress.style.borderWidth = '2px';
-    }
-}
