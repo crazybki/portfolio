72abe9f29d4aa8fe1483bd7301291d61809e56a1
crazybki <41273107+crazybki@users.noreply.github.com>
2021-03-26 18:57:45 +0100

    css updates index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+
+
 const bodyColor = document.querySelector('.containerbtn__btn');
 let imgElements = document.querySelector('.containerWork');
 const spacexImg = '/img/spaceX.jpg';
@@ -20,3 +23,46 @@ function backgroundColor() {
     }, 7800);
 
 }
+.containerWork {
+    transition: opacity 1s ease-in-out;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 4rem;
+}
+
+.changebackground {
+    opacity: 1;
+    animation-name: fadeInOpacity;
+    animation-duration: 4s;
+    animation-timing-function: ease-in-out;
+    width: 40%;
+}
+
+.changebackground2 {
+    opacity: 1;
+    animation-name: fadeInOpacity;
+    animation-duration: 4s;
+    animation-timing-function: ease-in-out;
+    width: 40%;
+}
+
+ fadeInOpacity {
+    0% {
+        opacity: 0;
+    }
+
+    50% {
+        opacity: 1;
+    }
+
+
+    100% {
+        opacity: 0;
+    }
+}
+
+        <div class="containerWork">
+        </div>
+ *
+ */
+
