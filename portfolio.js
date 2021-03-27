a2cac4cb50dc1695b9ac23ff4abd742fb5e68852
crazybki <41273107+crazybki@users.noreply.github.com>
2021-03-27 12:59:24 +0100

    added css and js to portfolio page

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,33 @@
 const page = document.querySelector('.containersnap__gameofthrones');
 const section = document.querySelector('html');
 const btn = document.querySelector('.btn-down');
+let position = 0;
 
 btn.addEventListener('click', scrollPage);
 
 function scrollPage() {
 
-    if (window.scrollY === 0) {
-        console.log('down');
+    if (window.scrollY === position) {
         page.scrollIntoView({ behavior: 'smooth' })
         setTimeout(() => {
             btn.innerHTML = `<p><i class="fas fa-arrow-circle-up fa-4x"></i></p>`;
         }, 500);
     }
     else {
-        console.log('up')
         section.scrollIntoView({ behavior: 'smooth' });
         setTimeout(() => {
             btn.innerHTML = `<p><i class="fas fa-arrow-circle-down fa-4x"></i></p>`;
         }, 500);
     }
 };
+
+
+window.addEventListener('scroll', scrollToPage);
+
+function scrollToPage() {
+    if (window.scrollY < 400) {
+        btn.innerHTML = `<p><i class="fas fa-arrow-circle-down fa-4x"></i></p>`;
+    } else {
+        btn.innerHTML = `<p><i class="fas fa-arrow-circle-up fa-4x"></i></p>`;
+    }
+}
